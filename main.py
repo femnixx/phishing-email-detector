f769368484530b83ad42dc8a1ec9ed64d821a30b
femnixx <femnitality@gmail.com>
2026-04-06 20:06:52 +0700

    feat: made classification report for ai prediction model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import confusion_matrix, classification_report
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(base_dir, "phishing_email.csv")
@@ -16,12 +17,19 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 vectorizer = TfidfVectorizer(stop_words='english')
-X_train_vec = vectorizer.fit(X_train)
+X_train_vec = vectorizer.fit_transform(X_train)
 X_test_vec = vectorizer.transform(X_test)
 
 model = MultinomialNB()
 
 model.fit(X_train_vec, y_train)
-
+y_pred = model.predict(X_test_vec)
 accuracy = model.score(X_test_vec, y_test)
+
 print(f"AI logic accuracy is: {accuracy * 100:.2f}% accurate.")
+
+print("\n--- Confusion Matrix ---")
+print(confusion_matrix(y_test, y_pred))
+
+print("\n--- Detailed Classification Report ---")
+print(classification_report(y_test, y_pred))
