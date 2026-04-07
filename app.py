224db8c63b89b224083bec1c866f44eef3c039ad
femnixx <femnitality@gmail.com>
2026-04-07 09:21:32 +0700

    fix: made feedback panel after email checking

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 import joblib
 from flask import Flask, request, jsonify, render_template
+import sqlite3
 
 app = Flask(__name__)
 
 model = joblib.load('model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')
 
+def init_db():
+    conn = sqlite3.connect('feedback.db')
+    conn.execute('''
+                 CREATE TABLE IF NOT EXISTS feedback (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    email_text TEXT,
+                    prediction TEXT,
+                    was_correct BOOLEAN,
+                 timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+                 )
+                 ''')
+    conn.commit()
+    conn.close()
+
+init_db()
+
 @app.route('/')
 def index():
-    return render_template('index.html')\
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -26,6 +43,17 @@ def predict():
         'result': 'PHISHING' if guess == 1 else 'SAFE',
         'probability': round(prob[1] * 100, 2)
     })
+
+@app.route('/feedback', methods=['POST'])
+def feedback(): 
+    data = request.get_json()
+    conn = sqlite3.connect('feedback.db')
+    conn.execute(
+        'INSERT INTO feedback (email_text, prediciton, was_correct) VALUES (?, ?, ?)', (data['text'], data['prediction'], data['was_correct'])
+    )
+    conn.commit()
+    conn.close()
+    return jsonify({ 'status': 'ok' })
 
 if __name__ == '__main__':
     app.run(debug=True)
